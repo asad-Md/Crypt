e4fca752b57b9b9332d31b607d0be5e86850f491
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2023-12-14 23:04:03 +0530

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,8 +1,9 @@
 export default function Background(){
     return(<>
     <video id="background-video" autoplay loop muted>
-        <source src="./src/assets/backgroundvideo.mp4" type="video/mp4" />
+        <source src="https://cdn.discordapp.com/attachments/1156918661550977106/1184909903173791854/backgroundvideo.mp4?ex=658db057&is=657b3b57&hm=66bca24e623d7c69e21fb8e4769385d0b88f9630988373b47d8a3caae0616f78&" type="video/mp4" />
     </video>
-    <img id="backgroundImg" src="../assets/background.png" />
+    <img id="backgroundImg" 
+        src="https://cdn.discordapp.com/attachments/1156918661550977106/1184909902754369747/background.png?ex=658db057&is=657b3b57&hm=9cc8935f4491e591c377487298680ca7070d68aea991fce5d430dc3f50fcae72&" />
     </>)
 }
