637464f64d4e015bb5438abb0fb3c02fe3039b97
N0Uchan <142255222+N0Uchan@users.noreply.github.com>
2023-12-15 17:27:58 +0530

    autoplay-> autoPlay

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,7 @@
 import bgVid from '../assets/backgroundvideo.mp4'
 export default function Background(){
     return(<>
-    <video id="background-video" autoplay loop muted>
+    <video id="background-video" autoPlay loop muted>
         <source src={bgVid} type="video/mp4" />
     </video>
     
