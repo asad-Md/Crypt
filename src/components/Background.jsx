5917de76c4742b0d0206579f80ce7600ce21a14d
Mohammed Asadullah <142255222+N0Uchan@users.noreply.github.com>
2023-12-15 16:45:58 +0530

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,6 @@ export default function Background(){
     <video id="background-video" autoplay loop muted>
         <source src="../assets/backgroundvideo.mp4" type="video/mp4" />
     </video>
-    <img id="backgroundImg" 
-        src="https://cdn.discordapp.com/attachments/1156918661550977106/1184909902754369747/background.png?ex=658db057&is=657b3b57&hm=9cc8935f4491e591c377487298680ca7070d68aea991fce5d430dc3f50fcae72&" />
+    
     </>)
 }
